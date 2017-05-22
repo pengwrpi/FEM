115dbf079bbfeaaa165add64d42e438044051825
pengwrpi <wei@wei-Proliant-ML10-v2.dynamic.rpi.edu>
2017-05-22 12:04:19 -0400

    delete unwanted files

--- matlabcode/testbeam.m
+++
@@ -1,7 +0,0 @@
-clear all;
-num_node = 200;
-fid = fopen('beam_displacement.txt','r');
-coord = fscanf(fid, '%d %f %f', [3, num_node]);
-fclose(fid);
-
-plot((num_node - 2)/2:-1:0, coord(3, 1:2:num_node), 'o')
